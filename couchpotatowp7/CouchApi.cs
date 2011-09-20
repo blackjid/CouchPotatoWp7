1267985c8a27b0b8f7afa8b6cbbda89a1b81d35a
Juan Ignacio Donoso <jidonoso@gmail.com>
2011-09-20 00:15:25 -0300

    Backup

---
+++ couchpotatowp7/CouchApi.cs
@@ -0,0 +1,99 @@
+﻿using System;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Ink;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+
+namespace couchpotatowp7
+{
+    public class CouchApi
+    {
+        // The settings
+        private string sHost;
+        private string sApiKey;
+        private string sPort;
+        private string sUsername;
+        private string sPassword;
+
+        // Base for every call to the api
+        private string sBaseUrl;
+
+        // The api methods
+        const string MovieListMethod = "movie.list";
+
+        /// <summary>
+        /// Get or set the API Key to be used by all calls. API key is mandatory for all 
+        /// calls to Couch.
+        /// </summary>
+        public string ApiKey
+        {
+            get { return sApiKey; }
+            set
+            {
+                sApiKey = value == null || value.Length == 0 ? null : value;
+            }
+        }
+
+        /// <summary>
+        /// Get or set the HOST to be used by all calls. HOST is mandatory for all 
+        /// calls to Couch.
+        /// </summary>
+        public string Host
+        {
+            get { return sHost; }
+            set
+            {
+                sHost = value == null || value.Length == 0 ? null : value;
+            }
+        }
+
+        /// <summary>
+        /// Get or set the HOST to be used by all calls. HOST is mandatory for all 
+        /// calls to Couch.
+        /// </summary>
+        public string Port
+        {
+            get { return sPort; }
+            set
+            {
+                sPort = value == null || value.Length == 0 ? null : value;
+            }
+        }
+
+        /// <summary>
+        /// Get the base url for all calls
+        /// </summary>
+        /// <returns></returns>
+        public string BaseUrl()
+        {
+            if (sHost == null || sHost.Length == 0
+                || sPort == null || sPort.Length == 0
+                || sApiKey == null || sApiKey.Length == 0)
+                sBaseUrl = null;
+            else
+                sBaseUrl = "http://" + sHost + ":" + sPort + "/" + sApiKey + "/";
+
+                return sBaseUrl; 
+        }
+
+        /// <summary>
+        /// Constructor
+        /// </summary>
+        public CouchApi()
+        { 
+        
+        }
+
+        public Uri MovieList() 
+        {
+            return new Uri(BaseUrl() + MovieListMethod);
+        }
+
+
+    }
+}
---
+++ couchpotatowp7/MainPage.xaml.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Animation;
+using System.Windows.Shapes;
+using Microsoft.Phone.Controls;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using couchpotatowp7.ViewModel;
+using Microsoft.Phone.Shell;
+
+namespace couchpotatowp7
+{
+    public partial class MainPage : PhoneApplicationPage
+    {
+
+        private MovieViewModel vm;
+
+        private AppSettings settings;
+                
+        // Constructor
+        public MainPage()
+        {
+            InitializeComponent();
+
+            settings = new AppSettings();
+            
+            //vm = new MovieViewModel();
+
+        }
+
+        protected override void OnNavigatedTo(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedTo(e);
+
+            if (settings.ApiKeySetting == "" || settings.HostSetting == "") {
+                NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.Relative));
+                return;
+            }
+
+            //vm.GetMovies();
+            //wantendList.DataContext = vm.WantedMovies;
+        }
+
+        protected override void OnNavigatedFrom(System.Windows.Navigation.NavigationEventArgs e)
+        {
+            base.OnNavigatedFrom(e);
+
+        }
+        
+        private void AppBarNewMovie_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/AddMovie.xaml", UriKind.Relative));
+        }
+        private void AppBarSettings_Click(object sender, EventArgs e)
+        {
+            NavigationService.Navigate(new Uri("/SettingsPage.xaml", UriKind.Relative));
+        }
+
+        private void Pivot_SelectionChanged(object sender, SelectionChangedEventArgs e)
+        {
+            switch (((Pivot)sender).SelectedIndex)
+            {
+                case 0:
+                    ApplicationBar = ((ApplicationBar)this.Resources["wantedAppBar"]);
+                    break;
+
+                case 1:
+                    ApplicationBar = ((ApplicationBar)this.Resources["snatchedAppBar"]);
+                    break;
+
+                case 2:
+                    ApplicationBar = ((ApplicationBar)this.Resources["downloadedAppBar"]);
+                    break;
+            }
+        }
+
+        
+    }
+}
